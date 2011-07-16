c8b6bdcaf7f76852901664a61368f0bcbb05f22f
Kuniyoshi Kouji <kuniyoshi@cpan.org>
2011-07-16 12:45:08 +0900

    initial commit

---
+++ lib/WWW/BOOKSCAN.pm
@@ -0,0 +1,32 @@
+package WWW::BOOKSCAN;
+use strict;
+use warnings;
+our $VERSION = '0.01';
+
+1;
+__END__
+
+=head1 NAME
+
+WWW::BOOKSCAN -
+
+=head1 SYNOPSIS
+
+  use WWW::BOOKSCAN;
+
+=head1 DESCRIPTION
+
+WWW::BOOKSCAN is
+
+=head1 AUTHOR
+
+kuniyoshi kouji E<lt>kuniyoshi@cpan.orgE<gt>
+
+=head1 SEE ALSO
+
+=head1 LICENSE
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=cut
